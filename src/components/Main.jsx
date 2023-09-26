53847f355ac26c49d187e77fcbdf82025a703b95
JakubCzajor <jakub.czajor7@gmail.com>
2023-09-26 23:44:04 +0200

    Added sidebar and working price inputs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Spinner from 'react-bootstrap/Spinner';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -6,22 +7,39 @@ import ReactPaginate from 'react-paginate';
 
 export default function Main(props) {
 
+    const [fromPrice, setFromPrice] = useState('');
+    const [toPrice, setToPrice] = useState('');
+
+    function handlePriceChange(event, type) {
+        const result = event.target.value.replace(/\D/g, '')
+        type === "from" ? setFromPrice(result) : setToPrice(result)
+    }
+
     return (
-        <main>
-            <Container className='main--options-container'>
-                <Form.Select value={props.sort} onChange={props.sortItems} className='main--select-sort'>
-                    <option value='featured'>Featured</option>
-                    <option value='price-asc'>Price ascending</option>
-                    <option value='price-desc'>Price descending</option>
-                    <option value='rating'>Highest Rating</option>
-                </Form.Select>
-                <Form.Select value={props.itemsPerPage} onChange={props.changeItemsPerPage} className='main--select-items'>
-                    <option value={6}>6</option>
-                    <option value={9}>9</option>
-                    <option value={12}>12</option>
-                </Form.Select>
+        <main className='main container'>
+            <Container className='main--sidebar'>
+                <Form>
+                <Form.Group>
+                    <Form.Label>Price</Form.Label>
+                    <Form.Control type="text" placeholder="from" value={fromPrice} onChange={(event) => handlePriceChange(event, "from")} /> -
+                    <Form.Control type="text" placeholder="to" value={toPrice} onChange={(event) => handlePriceChange(event, "to")} />
+                </Form.Group>
+                </Form>
             </Container>
             <Container className='main--items-container'>
+                <Container className='main--options-container'>
+                    <Form.Select value={props.sort} onChange={props.sortItems} className='main--select-sort'>
+                        <option value='featured'>Featured</option>
+                        <option value='price-asc'>Price ascending</option>
+                        <option value='price-desc'>Price descending</option>
+                        <option value='rating'>Highest Rating</option>
+                    </Form.Select>
+                    <Form.Select value={props.itemsPerPage} onChange={props.changeItemsPerPage} className='main--select-items'>
+                        <option value={6}>6</option>
+                        <option value={9}>9</option>
+                        <option value={12}>12</option>
+                    </Form.Select>
+                </Container>
                 <Row>
                     {props.loading && <Spinner className='loading-spinner' animation='border' variant='primary' />}
                     {props.itemElements}
